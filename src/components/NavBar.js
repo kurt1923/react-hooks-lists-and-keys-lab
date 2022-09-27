127cac5e0f0756e0eaca08dfd6d1dc6fb7d0beeb
Kurt Vermillion <kurtv0727@gmail.com>
2022-09-27 16:30:43 -0400

    lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,11 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  
+  const linksLis = links.map((link) => {
+  return <a key= {"#"+link} id = {link} href ={"#"+link}>{link}</a>})
+  
+  return linksLis
 }
 
 export default NavBar;
